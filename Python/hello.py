dfde6bfd064cad871b74262aa2236a34f87ac89a
Hitesh261102 <hitesh1126sharma@gmail.com>
2026-06-03 09:25:24 +0530

    Initial commit

---
+++ Python/hello.py
@@ -0,0 +1,73 @@
+# print("hello world")
+# a=10
+# name="Hitesh"
+# print(a)
+# print(name)
+# a="This is a string"
+# print(type(a))
+# slicing
+# print(len(a[2:6]))
+# print(a[-1])
+# print(a.upper())
+# print(name.lower())
+# print(name.title())
+# print(a.capitalize()) #diff b/w title and capitalize
+# print(a.casefold())
+# print(a.count("i"))# count occurence of letter
+# print(a.index("i"))# tells idx of letter
+
+# intro="I love java"
+# intro.replace("java","python")# replace word with another
+# print(intro)
+
+# string="python"
+# print(string.startswith("py"))# checks word start with 
+# print(string.endswith("on"))
+
+# num="123"
+# print(num.isdigit())
+# char="hello"
+# print(char.isalpha())
+
+# ch="is 123 numeric "
+# print(ch.isalnum())
+# print(ch*3)
+# print(ch+ch)
+# print(ch.strip())
+
+# string formating
+# name="abc"
+# address="pilani"
+# print(f"Hello My name is {name} and I am from {address}")
+
+
+#     list
+# 1.list is a collection of items
+# 2.hetrogenous
+# 3.ordered
+# 4.allow duplicate values
+
+list=[1,2,3,3,"hello"]
+# print(list)
+# print(type(list))
+# print(len(list))
+# print(list[0:(len(list)):2])
+# list[3]=4
+# print(list)
+# list.append(5)
+# list.insert(3,2)
+# list.extend([5,6,7,[1,2,[1,2]]])
+# print(list[8][2][1])
+# list.pop(3)
+# list.remove(1)
+# print(list.count(3))
+# list.clear()
+# print(list.copy())
+# list.reverse()
+# list.sort()
+
+# print(list)
+lst1=[1,2,3]
+lst2=[2,3,4]
+print(lst1+lst2)
+print(lst1*2)
